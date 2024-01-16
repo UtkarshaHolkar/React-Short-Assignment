9fedb45b56d3c5ba673292db22c8118170a3315d
UtkashaHolakr <utkarshaaholkar@gmail.com>
2024-01-16 22:02:19 +0530

    fourth  commit

--- project_02/src/App.jsx
+++ project_02/src/App.jsx
@@ -1,12 +1,19 @@
-
 import './App.css'
+import Card from './components/Card'
 
 function App() {
  
+  let myobj1 = {
+    name: "hi ",
+    age : 29
+  }
+  let myArr = [1,2,3];
 
   return (
     <>
-<h1>Tailwind css test</h1>
+<h1 className='bg-slate-400 text-black p-4 rounded-xl mb-4'>Tailwind css test</h1>
+<Card username = "utk" newone = {myobj1} />
+<Card username = "sha" newone = {myArr} />
     </>
   )
 }
