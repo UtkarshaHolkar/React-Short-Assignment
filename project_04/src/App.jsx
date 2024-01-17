f0ea565a0114a290b8a1574129d76b4464cfd36c
UtkashaHolakr <utkarshaaholkar@gmail.com>
2024-01-17 21:35:09 +0530

    sixth  commit

--- project_04/src/App.jsx
+++ project_04/src/App.jsx
@@ -1,14 +1,20 @@
-import { useState, useCallback } from 'react'
+import { useState, useCallback , useEffect, useRef} from 'react'
 
 
 // useCallback hook is used to optimize the code.
 // Basically this hook will store the function in (cache/memory) and jitna use lgra h krlo baaki k nhi
+
+
 
 function App() {
   const [length, setLength] = useState(8);
   const [numberAllowed, setNumberallowed] =useState(false);
   const [characterAllowed, setCharacterallowed] =useState(false);
   const [password, setPassword] =useState("");
+
+  // kisi bhi chiz k jb reference lena ho toh useref hook use hga
+
+  const passwordRef = useRef(null)
 
   const passwordGenerator = useCallback(()=>{
     let pass = ""
@@ -17,19 +23,33 @@ function App() {
      if (numberAllowed) str += "0123456789"
      if( characterAllowed) str += "!@#$%^&*_-+=[]{}~`"
 
-     for (let i = 1; i <= array.length; i++) {
+     for (let i = 1; i <=  length; i++) {
       let char = Math.floor(Math.random()* str.length + 1)
-      pass = str.charAt(char);
+      pass += str.charAt(char);
      }
 
      setPassword(pass);
 
   }, [length, numberAllowed, characterAllowed, setPassword])
+  //setPassword yaha pr optimization k liye use kiya h memoization k concept 
+
+const copyPasswordToClipboard = useCallback(()=>{
+
+  // useref hook is used here, this is going to select the text whatever we have copied.
+
+  passwordRef.current?.select
+  
+
+  // This below line is used to copy that password we are generating
+     window.navigator.clipboard.writeText(password)
+},[password]);
+
+  useEffect(()=>{passwordGenerator()},[length,  numberAllowed, characterAllowed, passwordGenerator])
 
   return (
     <>
       
-      <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 text-orange-500 bg-gray-700'>
+      <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 text-red-400 bg-gray-700'>
       <h1 className=' text-white text-center my-3'>Password generator</h1><div className='flex shadow rounded-lg overflow-hidden mb-4'>
          <input 
          type='text'
@@ -37,8 +57,9 @@ function App() {
          className='outline-none w-full py-1 px-3'
          placeholder='password'
          readOnly
+         ref={passwordRef}
          />
-         <button className='outline-none bg-blue-800 text-white px-3 py-0.5 shrink-0'>copy</button>
+         <button onClick={copyPasswordToClipboard} className='outline-none bg-blue-800 text-white px-3 py-0.5 shrink-0'>copy</button>
       </div>
       <div className='flex text-sm gap-x-2'>
         <div className='flex items-center gap-x-1'>
