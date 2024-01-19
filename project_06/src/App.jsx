d843239a0a8d372239e278b371d667dca6c7cdef
UtkashaHolakr <utkarshaaholkar@gmail.com>
2024-01-19 12:09:23 +0530

    eighth commit

--- project_06/src/App.jsx
+++ project_06/src/App.jsx
@@ -1,11 +1,14 @@
-// import { useState } from 'react'
+// // import { useState } from 'react'
 import './App.css'
+// import Footer from './components/Footer/Footer'
+// import Header from './components/Header/Header'
+// import Home from './components/Home/Home'
 
 function App() {
   
   return (
    <>
-<h1>hi</h1>
+
    </>
   )
 }
